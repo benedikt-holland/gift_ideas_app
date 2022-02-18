f20bf955acff90b383cdd351aa917a3317f3a1db
Lena Schelberger <lenchen120101@gmail.com>
2022-02-18 16:21:13 +0100

    Recycler View
    Recyclerview  with tiles implemented, prepared search bar

--- app/src/main/java/com/example/geschenkapp/MainActivity.kt
+++ app/src/main/java/com/example/geschenkapp/MainActivity.kt
@@ -1,19 +1,47 @@
 package com.example.geschenkapp
 
+import CustomAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        initProfileViewPager()
-    }
+    class MainActivity : AppCompatActivity() {
+        private lateinit var linearLayoutManager: LinearLayoutManager
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            // getting the recyclerview by its id
+            val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
 
-    private fun initProfileViewPager() {
-        var viewPager : ViewPager2 = findViewById(R.id.profileViewPager)
-        var adapter = ViewPAger
+            // this creates a vertical layout Manager
+            recyclerview.layoutManager = LinearLayoutManager(this)
+
+            // ArrayList of class ItemsViewModel
+            val data = ArrayList<ItemsViewModel>()
+
+            // This loop will create 20 Views containing
+            // the image with the count of view
+            for (i in 1..20) {
+                data.add(ItemsViewModel(R.drawable.ic_profile, "Item " + i))
+            }
+
+            // This will pass the ArrayList to our Adapter
+            val adapter = CustomAdapter(data)
+
+            // Setting the Adapter with the recyclerview
+            recyclerview.adapter = adapter
+
+
+            initProfileViewPager()
+        }
+
+        private fun initProfileViewPager() {
+            var viewPager : ViewPager2 = findViewById(R.id.profileViewPager)
+            //var adapter = ViewPAger
+        }
     }
 }
