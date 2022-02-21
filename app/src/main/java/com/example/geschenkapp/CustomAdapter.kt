92a27590dcf190d8695d78d6f1fb16bcb0816575
Lena Schelberger <lenchen120101@gmail.com>
2022-02-21 12:17:38 +0100

    search view
    implemented search view with test array

--- app/src/main/java/com/example/geschenkapp/CustomAdapter.kt
+++ app/src/main/java/com/example/geschenkapp/CustomAdapter.kt
@@ -1,16 +1,21 @@
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Filter
-import android.widget.Filterable
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.example.geschenkapp.ItemsViewModel
 import com.example.geschenkapp.R
+import java.util.*
+import kotlin.collections.ArrayList
 
-class CustomAdapter(private var mList: List<ItemsViewModel>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+//class CustomAdapter(private var mList: ArrayList<ItemsViewModel>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>(), Filterable {
+class CustomAdapter(private var testList: ArrayList<String>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>(), Filterable {
 
+    //test array
+    var testFilterList = ArrayList<String>()
+    init {
+        testFilterList = testList
+    }
     // create new views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflates the card_view_design view
@@ -24,26 +29,58 @@ class CustomAdapter(private var mList: List<ItemsViewModel>) : RecyclerView.Adap
     // binds the list items to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        val ItemsViewModel = mList[position]
+        //val ItemsViewModel = mList[position]
+        //holder.bind(mList[position])
+        holder.bind(testFilterList[position])
 
         // sets the image to the imageview from our itemHolder class
         //holder.imageView.setImageResource(ItemsViewModel.image)
 
         // sets the text to the textview from our itemHolder class
-        holder.textView.text = ItemsViewModel.text
+        //holder.textView.text = ItemsViewModel.text
 
     }
 
     // return the number of the items in the list
     override fun getItemCount(): Int {
-        return mList.size
+        //return mList.size
+        return testFilterList.size
     }
 
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                if (charSearch.isEmpty()) {
+                    testFilterList = testList
+                } else {
+                    val resultList = ArrayList<String>()
+                    for (row in testList) {
+                        if (row.lowercase(Locale.ROOT).contains(charSearch.lowercase(Locale.ROOT))) {
+                            resultList.add(row)
+                        }
+                    }
+                    testFilterList = resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = testFilterList
+                return filterResults
+            }
 
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                testFilterList = results?.values as ArrayList<String>
+                notifyDataSetChanged()
+            }
+
+        }
+    }
     // Holds the views for adding it to image and text
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        //val imageView: ImageView = itemView.findViewById(R.id.imageview)
-        val textView: TextView = itemView.findViewById(R.id.textView)
+        fun bind(mList: String) {
+            //val imageView: ImageView = itemView.findViewById(R.id.imageview)
+            val textView: TextView = itemView.findViewById(R.id.textView)
+        }
     }
 
 
--- app/src/main/java/com/example/geschenkapp/MainActivity.kt
+++ app/src/main/java/com/example/geschenkapp/MainActivity.kt
@@ -3,17 +3,47 @@ package com.example.geschenkapp
 import CustomAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
 import androidx.viewpager2.widget.ViewPager2
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.geschenkapp.databinding.ActivityMainBinding
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     class MainActivity : AppCompatActivity() {
+        lateinit var adapter: CustomAdapter
+        lateinit var rv: RecyclerView
+        private lateinit var binding: ActivityMainBinding
+
         private lateinit var linearLayoutManager: LinearLayoutManager
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+            //setContentView(R.layout.activity_main)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            val view = binding.root
+            setContentView(view)
+
+            rv = findViewById(R.id.recyclerview)
+            rv.layoutManager = LinearLayoutManager(rv.context)
+            rv.setHasFixedSize(true)
+
+            binding.search.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    adapter.filter.filter(newText)
+                    return false
+                }
+
+            })
+
+            getListOfTest()
+            /**
             // getting the recyclerview by its id
             val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
 
@@ -34,7 +64,7 @@ class MainActivity : AppCompatActivity() {
 
             // Setting the Adapter with the recyclerview
             recyclerview.adapter = adapter
-
+            **/
 
             initProfileViewPager()
         }
@@ -43,5 +73,25 @@ class MainActivity : AppCompatActivity() {
             var viewPager : ViewPager2 = findViewById(R.id.profileViewPager)
             //var adapter = ViewPAger
         }
+
+        private fun getListOfTest() {
+            val isoCountryCodes = Locale.getISOCountries()
+            val countryListWithEmojis = ArrayList<String>()
+            for (countryCode in isoCountryCodes) {
+                val locale = Locale("", countryCode)
+                val countryName = locale.displayCountry
+                val flagOffset = 0x1F1E6
+                val asciiOffset = 0x41
+                val firstChar = Character.codePointAt(countryCode, 0) - asciiOffset + flagOffset
+                val secondChar = Character.codePointAt(countryCode, 1) - asciiOffset + flagOffset
+                val flag =
+                    (String(Character.toChars(firstChar)) + String(Character.toChars(secondChar)))
+                countryListWithEmojis.add("$countryName $flag")
+            }
+            adapter = CustomAdapter(countryListWithEmojis)
+            rv.adapter = adapter
+        }
+
+
     }
 }
