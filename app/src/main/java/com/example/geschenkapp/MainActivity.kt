f8f831ae14a4b93529a245246c804843654f3e3e
Lena Schelberger <lenchen120101@gmail.com>
2022-02-21 14:26:51 +0100

    search functionality
    added search functionality to search view

--- app/src/main/java/com/example/geschenkapp/MainActivity.kt
+++ app/src/main/java/com/example/geschenkapp/MainActivity.kt
@@ -67,20 +67,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getListOfTest() {
-        val isoCountryCodes = Locale.getISOCountries()
-        val countryListWithEmojis = ArrayList<String>()
-        for (countryCode in isoCountryCodes) {
-            val locale = Locale("", countryCode)
-            val countryName = locale.displayCountry
-            val flagOffset = 0x1F1E6
-            val asciiOffset = 0x41
-            val firstChar = Character.codePointAt(countryCode, 0) - asciiOffset + flagOffset
-            val secondChar = Character.codePointAt(countryCode, 1) - asciiOffset + flagOffset
-            val flag =
-                (String(Character.toChars(firstChar)) + String(Character.toChars(secondChar)))
-            countryListWithEmojis.add("$countryName $flag")
+        val testListAbc = ArrayList<String>()
+        for (i in 1..20) {
+            testListAbc.add("item $i")
         }
-        adapter = CustomAdapter(countryListWithEmojis)
+        adapter = CustomAdapter(testListAbc)
         rv.adapter = adapter
     }
 
