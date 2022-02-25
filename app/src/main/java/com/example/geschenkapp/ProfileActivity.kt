ac6a3ade3a23387754af062c382613db76df3b78
Diego <diego.mv@gmx.de>
2022-02-25 15:49:54 +0100

    Added Activities for Profile and Notification

---
+++ app/src/main/java/com/example/geschenkapp/ProfileActivity.kt
@@ -0,0 +1,10 @@
+package com.example.geschenkapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
