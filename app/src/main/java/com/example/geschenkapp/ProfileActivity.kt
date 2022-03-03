b6b15858e1f18d87379bc95bb119b3e56fa5870a
Diego <diego.mv@gmx.de>
2022-03-03 22:50:03 +0100

    Cleaned up Code for ProfileActivity

--- app/src/main/java/com/example/geschenkapp/ProfileActivity.kt
+++ app/src/main/java/com/example/geschenkapp/ProfileActivity.kt
@@ -1,24 +1,11 @@
 package com.example.geschenkapp
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.load.model.LazyHeaders
-import com.example.geschenkapp.data.User
 import com.example.geschenkapp.databinding.ActivityProfileBinding
-import com.squareup.picasso.Picasso
 import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.Main
-import okhttp3.*
-import okhttp3.internal.http2.Header
-import java.io.File
-import java.io.InputStream
 import java.util.*
 
 
