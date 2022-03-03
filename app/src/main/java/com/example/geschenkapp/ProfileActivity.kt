989090f20178af43491c4601878d2959a52e4b5e
Lena Schelberger <lenchen120101@gmail.com>
2022-03-03 19:37:27 +0100

    implemented swipeable tab view in profile

--- app/src/main/java/com/example/geschenkapp/ProfileActivity.kt
+++ app/src/main/java/com/example/geschenkapp/ProfileActivity.kt
@@ -2,10 +2,31 @@ package com.example.geschenkapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+val tabArray = arrayOf(
+    "Wunschliste",
+    "Events",
+    "Freunde"
+)
 
 class ProfileActivity : AppCompatActivity() {
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        tabLayout = findViewById(R.id.profileTabLayout)
+        viewPager = findViewById(R.id.profileViewPager)
+
+        val adapter = TabAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabArray[position]
+        }.attach()
     }
 }
