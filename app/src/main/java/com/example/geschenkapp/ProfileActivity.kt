3dc41c6fbb8da02a43e879a876094321974738d2
Lena Schelberger <lenchen120101@gmail.com>
2022-03-03 22:23:27 +0100

    fixed notification activity

--- app/src/main/java/com/example/geschenkapp/ProfileActivity.kt
+++ app/src/main/java/com/example/geschenkapp/ProfileActivity.kt
@@ -24,7 +24,7 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.apply {
-            title = ""
+            title = "Profil"
             // show back button on toolbar
             // on back button press, it will navigate to parent activity
             setDisplayHomeAsUpEnabled(true)
