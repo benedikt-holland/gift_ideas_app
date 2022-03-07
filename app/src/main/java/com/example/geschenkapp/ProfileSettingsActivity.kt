005cc3d450ecdd52f877afb8395a64cccb82e957
Diego <diego.mv@gmx.de>
2022-03-07 20:38:35 +0100

    Commit for Lena

--- app/src/main/java/com/example/geschenkapp/ProfileSettingsActivity.kt
+++ app/src/main/java/com/example/geschenkapp/ProfileSettingsActivity.kt
@@ -2,12 +2,17 @@ package com.example.geschenkapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import com.example.geschenkapp.databinding.ActivityMainBinding
+import com.example.geschenkapp.databinding.ActivityProfileSettingsBinding
 import java.sql.ResultSet
 
 class ProfileSettingsActivity : AppCompatActivity() {
 
     lateinit var user: ResultSet
     private var db = DbConnector()
+    private lateinit var binding: ActivityProfileSettingsBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +26,22 @@ class ProfileSettingsActivity : AppCompatActivity() {
             setDisplayShowHomeEnabled(true)
         }
 
+        binding = ActivityProfileSettingsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         user = DataHolder.getInstance().user
         db = DbHolder.getInstance().db
 
-        
+        val items = listOf("1", "2", "3", "4")
+        val adapter = ArrayAdapter(this, R.layout.list_profileprivacy_item, items)
+        (binding.tvPrivacy.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+
+        binding.tvFirstName.setText(user.getString("first_name"))
+        binding.tvLastName.setText(user.getString("last_name"))
+
+
+
 
     }
 }
