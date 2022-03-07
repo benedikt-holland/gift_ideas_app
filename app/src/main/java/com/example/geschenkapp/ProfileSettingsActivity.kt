b6fbbddb76a41f12ddda014cc158dfccf409ae89
Diego <diego.mv@gmx.de>
2022-03-07 18:58:11 +0100

    ProfileSettings simple Frontend

--- app/src/main/java/com/example/geschenkapp/ProfileSettingsActivity.kt
+++ app/src/main/java/com/example/geschenkapp/ProfileSettingsActivity.kt
@@ -2,12 +2,17 @@ package com.example.geschenkapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.sql.ResultSet
 
 class ProfileSettingsActivity : AppCompatActivity() {
+
+    lateinit var user: ResultSet
+    private var db = DbConnector()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_settings)
-
 
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.apply {
@@ -15,5 +20,11 @@ class ProfileSettingsActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
         }
+
+        user = DataHolder.getInstance().user
+        db = DbHolder.getInstance().db
+
+        
+
     }
 }
